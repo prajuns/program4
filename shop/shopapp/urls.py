2e7cf287aa5f7f5e223aea376de6e3d7b159787a
prajuns <130234119+prajuns@users.noreply.github.com>
2023-05-14 00:20:39 +0530

    Delete shop directory
    ok

--- shop/shopapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name= 'shopapp'
-
-urlpatterns =[
-    path('',views.allproduct,name="allproduct"),
-    path('<slug:c_slug>/',views.allproduct,name="pro by cat"),
-    path('<slug:c_slug>/<slug:product_slug>/',views.productde,name="product")
-]
